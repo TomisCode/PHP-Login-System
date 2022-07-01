975d357871724add1e7353f4d3665e323ced6726
Andrea Dybendal <103036045+TomisCode@users.noreply.github.com>
2022-07-01 14:07:58 +0200

    New files have been added

---
+++ login.php
@@ -0,0 +1,25 @@
+<div class="uk-section uk-container">
+        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+           <form class="uk-form-stacked js-login">
+  
+                <div class="uk-margin">
+                    <label class = "uk-form-label" for="form-stacked-text">Email</label>
+                    <div class="uk-form-controls">
+                        <input class ="uk-input" id="form-stacked-text" type="email" required='required' placeholder='you@email.com'>
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <label class = "uk-form-label" for="form-stacked-text">Password</label>
+                    <div class="uk-form-controls">
+                        <input class ="uk-input" id="form-stacked-text" type="password" required='required' placeholder='Your password'>
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-default" type="submit"></button>
+                </div>
+     
+            </form>
+        </div>
+  	</div>
