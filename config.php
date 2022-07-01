6e098b3a09380f8c21a7c1c9def27d0da4ee0b5c
Andrea Dybendal <103036045+TomisCode@users.noreply.github.com>
2022-07-01 14:09:09 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// If there is no constant defined called __CONFIG__, do not load file
+    if(!defined('__CONFIG__')) {
+        exit('You do not have a config file');
+    }
+
+    define('ALLOW FOOTER', true);
+    // My config is below
+
+    ?>
